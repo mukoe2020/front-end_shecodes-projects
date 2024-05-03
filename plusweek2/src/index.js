541c3210a1383199f05121f1ca14d270d394055d
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-05-03 14:48:49 +0200

    weather app changes

---
+++ plusweek2/src/index.js
@@ -0,0 +1,40 @@
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDate = new Date();
+let currentDateELement = document.querySelector("#current-date");
+currentDateELement.innerHTML = formatDate(currentDate);
