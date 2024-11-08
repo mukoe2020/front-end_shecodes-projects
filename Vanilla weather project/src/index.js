f3fa9e7ddd570145fa31b3f117c8ad5242198b2a
memoe <memoemukoe@gmail.com>
2024-11-08 21:39:04 +0200

    api intergratin done

--- Vanilla weather project/src/index.js
+++ Vanilla weather project/src/index.js
@@ -1,9 +1,29 @@
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input")
-  let cityElement = document.querySelector("#city")
-  cityElement.innerHTML= searchInput.value
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature")
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  
 }
 
-let searchFormElement = document.querySelector("#search-form")
+function searchCity(city) {
+  let apiKey = "5d35aed7d739od4f0tcba50250e7abc6";
+  let apiUrl =
+    `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather)
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon")
